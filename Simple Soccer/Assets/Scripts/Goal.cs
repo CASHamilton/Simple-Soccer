d5494db32333a54115f984f6bc3ed5d64df8f983
Cosmicshoelace <packratdrifters@gmail.com>
2018-10-03 13:56:19 -0700

    Started Development

---
+++ Simple Soccer/Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Soccer Ball")
+        {
+            Debug.Log("GOAL!");
+        }
+        else if (other.name == "Player")
+        {
+            Debug.Log("GET OUT OF THERE!");
+        }
+    }
+}
