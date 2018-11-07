8d9433594c42651ef8f907c3bcbfe6331f3b01d1
Cosmicshoelace <packratdrifters@gmail.com>
2018-11-07 10:30:19 -0800

    Alpha 1.0
    The open Alpha is Live!!! We are bringing you the biggest update yet! There is now a menu screen and an improved field.

---
+++ Simple Soccer/Assets/Scene Data/MenuActions.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Field 1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
+
