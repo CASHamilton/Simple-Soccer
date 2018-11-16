ac5974c67c63a1f79024a8bcd37f74eed4e57870
Cosmicshoelace <packratdrifters@gmail.com>
2018-11-15 16:27:30 -0800

    Started pause menu

---
+++ Simple Soccer/Assets/Scene Data/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseCanvas;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PauseCanvas.SetActive(true);
+        }
+
+    }
+    public void Resume()
+    {
+        //Debug.Log("Resume");
+        PauseCanvas.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
