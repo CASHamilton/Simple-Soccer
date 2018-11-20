1c5dbc21c26e75e0eb0006ca0fb661aee0b802b4
Cosmicshoelace <packratdrifters@gmail.com>
2018-11-20 14:18:22 -0800

    The next big update is here!
    The game is finally in Beta 2.0! We are bringing you Motorball! We have cleaned the modes up a bit and added some invisible walls so you wont fly off the map.

---
+++ Simple Soccer/Assets/Scene Data/Gamemodeactios.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemodeactios : MonoBehaviour
+{
+    public void Regular()
+    {
+        SceneManager.LoadScene("Field 1");
+    }
+    public void Motorball()
+    {
+        SceneManager.LoadScene("Motorball");
+    }
+
+
+
+}
---
+++ Simple Soccer/Assets/Scene Data/MenuActions.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game Mode");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
+
